6d783ce041d4ab9bdc2ee74d659aac0dbfe167fb
SunnyOst <90219234+SunnyOst@users.noreply.github.com>
2023-11-15 02:08:49 +0200

    Add files via upload
    added an example function to combat underscores in actual names

--- SteamCloudFilenameFix.py
+++ SteamCloudFilenameFix.py
@@ -6,7 +6,7 @@ import shutil
 # This script interprets EVERY underscore in the name as a slash in the path to the file.
 # IE "backup_001.txt" will be treated as "backup/001.txt" and will result in a file "001.txt" inside a folder "backup".
 # It is YOUR job to manually go through files and isolate files containing underscores in the name or the path.
-# You may rename and move incompatible files manually or modify this script to accomodate your specific situation if you're some sort of a genius.
+# You may rename and move incompatible files manually or modify this script to accomodate your specific situation if you're some sort of a genius. (Simple example function given)
 
 # I am not responsible for any kind of data loss or other issues that could arise from using this script. Use at your own risk!
 
@@ -23,6 +23,18 @@ source_dir = "joe mama's source directory"
 
 # Set the path to a folder where you want all the folders and files to end up at! By default, same folder as the source.
 dest_dir = source_dir
+
+# Here is a simple function to combat underscores when there is a lot of them present in a consistent manner:
+def manualrename(name):
+    # Here is an example of how you could remove false positive underscores (remove the #):
+    #name = name.replace("joe_mama","joeUNDERSCOREmama")
+    # This will reaplce EVERY instance of "joe_mama" with "joeUNDERSCOREmama", including situations where "joe/mama" path would be correct, so double check.
+    # You may add as many of these rules as you want inside this function, one per line
+    # !IMPORTANT! You will need to find a way to fix the names of these files/folders back to original names - whether manually or via another function! Otherwise, the files might not work correctly!
+
+
+    # End of function
+    return name
 
 # END OF SETUP
 
@@ -51,10 +63,11 @@ if not os.path.exists(dest_dir) or not os.path.isdir(dest_dir):
 
 # Loop through all files in the source directory
 for filename in files:
-    newfilename = filename
+    # Execute the de-underscorer function to remove potential issues
+    newfilename = manualrename(filename)
     
     # If steam has added a special folder before all the other folder names, format it to be the same as others
-    if filename.startswith("%"):
+    if newfilename.startswith("%"):
         newfilename = newfilename.replace("%","",1)
         newfilename = newfilename.replace("%","_",1)
     
